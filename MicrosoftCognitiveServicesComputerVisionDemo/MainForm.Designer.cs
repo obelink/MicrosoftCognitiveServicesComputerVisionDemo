2ab409bd50d402472c3d690e95db088fed445124
André Obelink <andre@obelink.com>
2017-07-22 16:33:37 -0400

    Initial commit

---
+++ MicrosoftCognitiveServicesComputerVisionDemo/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MicrosoftCognitiveServicesComputerVisionDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.selectImageFilePathButton = new System.Windows.Forms.Button();
+            this.jsonResultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // imageFilePathTextBox
+            // 
+            this.imageFilePathTextBox.Location = new System.Drawing.Point(40, 37);
+            this.imageFilePathTextBox.Multiline = true;
+            this.imageFilePathTextBox.Name = "imageFilePathTextBox";
+            this.imageFilePathTextBox.ReadOnly = true;
+            this.imageFilePathTextBox.Size = new System.Drawing.Size(745, 49);
+            this.imageFilePathTextBox.TabIndex = 0;
+            // 
+            // selectImageFilePathButton
+            // 
+            this.selectImageFilePathButton.Location = new System.Drawing.Point(792, 37);
+            this.selectImageFilePathButton.Name = "selectImageFilePathButton";
+            this.selectImageFilePathButton.Size = new System.Drawing.Size(75, 49);
+            this.selectImageFilePathButton.TabIndex = 1;
+            this.selectImageFilePathButton.Text = "...";
+            this.selectImageFilePathButton.UseVisualStyleBackColor = true;
+            this.selectImageFilePathButton.Click += new System.EventHandler(this.selectImageFilePathButton_Click);
+            // 
+            // jsonResultTextBox
+            // 
+            this.jsonResultTextBox.Location = new System.Drawing.Point(40, 105);
+            this.jsonResultTextBox.Multiline = true;
+            this.jsonResultTextBox.Name = "jsonResultTextBox";
+            this.jsonResultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.jsonResultTextBox.Size = new System.Drawing.Size(827, 581);
+            this.jsonResultTextBox.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 709);
+            this.Controls.Add(this.jsonResultTextBox);
+            this.Controls.Add(this.selectImageFilePathButton);
+            this.Controls.Add(this.imageFilePathTextBox);
+            this.Name = "MainForm";
+            this.Text = "Computer Vision Api - Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox imageFilePathTextBox;
+        private System.Windows.Forms.Button selectImageFilePathButton;
+        private System.Windows.Forms.TextBox jsonResultTextBox;
+    }
+}
+
---
+++ MicrosoftCognitiveServicesComputerVisionDemo/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MicrosoftCognitiveServicesComputerVisionDemo
+{
+    public partial class MainForm : Form
+    {
+        private const string SubscriptionKey = "your secret key";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void selectImageFilePathButton_Click(object sender, EventArgs e)
+        {
+            var imageFilePath = GetImageFilePath();
+
+            imageFilePathTextBox.Text = imageFilePath;
+
+            var json = await GetAnalyzeImageResult(imageFilePath);
+              
+            jsonResultTextBox.Text = FormatJson(json);
+        }
+
+        private static string GetImageFilePath()
+        {
+            var openFileDialog = new OpenFileDialog()
+            {
+                Filter = "jpg files (*.jpg)|*.jpg|All files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                return openFileDialog.FileName;
+            }
+
+            return string.Empty;
+        }
+
+        private static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (var fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var binaryReader = new BinaryReader(fileStream))
+                {
+                    return binaryReader.ReadBytes((int)fileStream.Length);
+                }
+            }
+        }
+
+        private static string GetRequestUri()
+        {
+            const string uriBase = "https://westeurope.api.cognitive.microsoft.com/vision/v1.0/analyze";
+            const string requestParameters = "visualFeatures=Categories,Tags,Description";
+
+            return uriBase + "?" + requestParameters;
+        }
+
+        private static async Task<string> GetAnalyzeImageResult(string imageFilePath)
+        {
+            var byteData = GetImageAsByteArray(imageFilePath);
+            var requestUri = GetRequestUri();
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
+
+                using (var content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    
+                    var response = await client.PostAsync(requestUri, content);
+
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+
+        private static string FormatJson(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            var stringBuilder = new StringBuilder();
+            var quote = false;
+            var ignore = false;
+            var offset = 0;
+            const int indentLength = 3;
+
+            foreach (var ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    stringBuilder.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            stringBuilder.Append(ch);
+                            stringBuilder.Append(Environment.NewLine);
+                            stringBuilder.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            stringBuilder.Append(Environment.NewLine);
+                            stringBuilder.Append(new string(' ', --offset * indentLength));
+                            stringBuilder.Append(ch);
+                            break;
+                        case ',':
+                            stringBuilder.Append(ch);
+                            stringBuilder.Append(Environment.NewLine);
+                            stringBuilder.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            stringBuilder.Append(ch);
+                            stringBuilder.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') stringBuilder.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
+    
